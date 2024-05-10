9f329e6ed942d5e16e2083ab7da6e7232251811f
OddEyed45 <sreejama33@gmail.com>
2024-05-10 04:45:06 -0500

    9th commit - Sreeja

--- src/main/java/com/example/projecttester/RaceController.java
+++ src/main/java/com/example/projecttester/RaceController.java
@@ -5,6 +5,7 @@ import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -13,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -81,29 +83,44 @@ public class RaceController implements Initializable
             flyDuck2.setVisible(false);
         }
 
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(secBetweenFlaps), event -> {
-            if (flyDuck != null && flyDuck2 != null){
-                flyDuck.setVisible(false);
-                flyDuck2.setVisible(true);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(secBetweenFlaps),
+                new EventHandler<ActionEvent>()
+                {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                if (flyDuck != null && flyDuck2 != null){
+                    flyDuck.setVisible(false);
+                    flyDuck2.setVisible(true);
+                }
             }
-
         }));
 
 
-        Timeline timeline2 = new Timeline(new KeyFrame(Duration.seconds(otherSecBetweenFlaps), event -> {
+        Timeline timeline2 = new Timeline(new KeyFrame(Duration.seconds(otherSecBetweenFlaps),
+                new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
 
-            if (flyDuck != null && flyDuck2 != null) {
-                flyDuck.setVisible(true);
-                flyDuck2.setVisible(false);
-                flyDuck.setX((flyDuck.getX() + 3 * (Math.pow(5 / energyLevel, -2) + 1 - energyLevel)));
-                flyDuck2.setX(flyDuck2.getX() + 3 * (Math.pow(5 / energyLevel, -2) + 1 - energyLevel));
+
+                if (flyDuck != null && flyDuck2 != null) {
+                    flyDuck.setVisible(true);
+                    flyDuck2.setVisible(false);
+                    flyDuck.setX((flyDuck.getX() + 3 * (Math.pow(5 / energyLevel, -2) + 1 - energyLevel)));
+                    flyDuck2.setX(flyDuck2.getX() + 3 * (Math.pow(5 / energyLevel, -2) + 1 - energyLevel));
+                }
             }
         }));
 
         PauseTransition pause = new PauseTransition(Duration.seconds(secBetweenFlaps));
 
-        pause.setOnFinished(event -> {
-            timeline2.play();
+        pause.setOnFinished(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                timeline2.play();
+            }
         });
 
         timeline.setCycleCount(Timeline.INDEFINITE);
@@ -116,35 +133,39 @@ public class RaceController implements Initializable
     {
         if (flyDuck != null) {
             flyDuck.setFocusTraversable(true);
-            flyDuck.setOnKeyPressed(event -> {
-                KeyCode code = event.getCode();
-                switch (code) {
-                    case UP:
-                        TranslateTransition translateUp = new TranslateTransition();
-                        translateUp.setByY(-60);
-                        translateUp.setNode(flyDuck);
+            flyDuck.setOnKeyPressed(new EventHandler<KeyEvent>()
+            {
+                @Override
+                public void handle(KeyEvent event) {
+                    KeyCode code = event.getCode();
+                    switch (code) {
+                        case UP:
+                            TranslateTransition translateUp = new TranslateTransition();
+                            translateUp.setByY(-60);
+                            translateUp.setNode(flyDuck);
 
-                        TranslateTransition translateUp2 = new TranslateTransition();
-                        translateUp2.setByY(-60);
-                        translateUp2.setNode(flyDuck2);
+                            TranslateTransition translateUp2 = new TranslateTransition();
+                            translateUp2.setByY(-60);
+                            translateUp2.setNode(flyDuck2);
 
-                        translateUp.play();
-                        translateUp2.play();
-                        break;
-                    case DOWN:
-                        TranslateTransition translateDown = new TranslateTransition();
-                        translateDown.setByY(60);
-                        translateDown.setNode(flyDuck);
+                            translateUp.play();
+                            translateUp2.play();
+                            break;
+                        case DOWN:
+                            TranslateTransition translateDown = new TranslateTransition();
+                            translateDown.setByY(60);
+                            translateDown.setNode(flyDuck);
 
-                        TranslateTransition translateDown2 = new TranslateTransition();
-                        translateDown2.setByY(60);
-                        translateDown2.setNode(flyDuck2);
+                            TranslateTransition translateDown2 = new TranslateTransition();
+                            translateDown2.setByY(60);
+                            translateDown2.setNode(flyDuck2);
 
-                        translateDown.play();
-                        translateDown2.play();
-                        break;
-                    default:
-                        break;
+                            translateDown.play();
+                            translateDown2.play();
+                            break;
+                        default:
+                            break;
+                    }
                 }
             });
         }
